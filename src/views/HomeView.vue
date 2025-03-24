f250d66632f73c569e40cd99f492d070c9d85de0
Joanne Hyun <joanne.hyun@MLHYWYV426CT.cardinalhealth.net>
2025-03-24 19:06:38 -0400

    cleanup homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,10 @@ import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-<<<<<<< HEAD
   <div>
     <h1>This is the home page</h1>
     <HelloWorld></HelloWorld>
   </div>
-=======
-  <main>
-    <!-- <TheWelcome /> -->
-  </main>
->>>>>>> 6f4f45f (includes vue scaffolding)
 </template>
 
 <style></style>
